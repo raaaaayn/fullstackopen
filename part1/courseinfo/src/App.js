f31b168ff03d4db34236d6435aaa96586082be0e
eafds <eafds@gmail.com>
2021-05-07 14:27:21 +0530

    1.5: course information step5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -36,31 +36,32 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content pack={parts} />
+      <Header course={course.name} />
+      <Content pack={course.parts} />
       <Total
-        n1={parts[0].exercises}
-        n2={parts[1].exercises}
-        n3={parts[2].exercises}
+        n1={course.parts[0].exercises}
+        n2={course.parts[1].exercises}
+        n3={course.parts[2].exercises}
       />
     </div>
   );
