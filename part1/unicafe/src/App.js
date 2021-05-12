8f46f48668154b679e9152039560f3d04c53c5f5
eafds <eafds@gmail.com>
2021-05-12 17:26:48 +0530

    1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,12 +4,28 @@ const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
+const Meth = ({ feedback, total, whattodo }) => {
+  if (whattodo === "avg") {
+    return (
+      <p>
+        Average resentment:
+        {(feedback.good - feedback.bad) / total}
+      </p>
+    );
+  }
+  if (whattodo === "p") {
+    return <p>Postive feedback: {(feedback.good / total) * 100}%</p>;
+  }
+};
+
 const App = () => {
   const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
   });
+
+  const total = feedback.good + feedback.neutral + feedback.bad;
   return (
     <div>
       <h1>Give feedback</h1>
@@ -48,6 +64,10 @@ const App = () => {
       <p>Good: {feedback.good}</p>
       <p>Neutral: {feedback.neutral}</p>
       <p>Bad: {feedback.bad}</p>
+      <p>All: {total}</p>
+
+      <Meth feedback={feedback} total={total} whattodo="avg" />
+      <Meth feedback={feedback} total={total} whattodo="p" />
     </div>
   );
 };
