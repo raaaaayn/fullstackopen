f6cc8defba20f41ecab7d8aee7202a57f1d4aa3e
eafds <eafds@gmail.com>
2021-05-31 03:02:31 +0530

    3.5/6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-app = express();
 
-const persons = [
+app = express();
+app.use(express.json());
+
+let persons = [
   {
     name: "Arto Hellas",
     number: "040-123456",
@@ -40,7 +42,7 @@ app.get("/api/info", (req, resp) => {
 });
 
 app.get("/api/persons/:id", (req, resp) => {
-  id = req.params.id;
+  const id = req.params.id;
   const person = persons.find((person) => person.id == id);
   if (person) {
     resp.json(person);
@@ -49,7 +51,39 @@ app.get("/api/persons/:id", (req, resp) => {
   }
 });
 
-const PORT = 3001;
+app.delete("/api/persons/:id", (req, resp) => {
+  const id = req.params.id;
+  const persontobedelete = persons.find((person) => person.id == id);
+  if (persontobedelete) {
+    console.log(persontobedelete, "has been deleted");
+    persons = persons.filter((person) => person.id != id);
+    resp.sendStatus(204).end();
+  } else {
+    console.log("person does not exist");
+    resp.sendStatus(204).end();
+  }
+});
+
+const generateid = () => Math.floor(Math.random() * 6999);
+
+app.post("/api/persons", (req, resp) => {
+  const newperson = {
+    name: req.body.name,
+    number: req.body.number,
+    id: generateid(),
+  };
+  if (newperson.name && newperson.number) {
+    if (persons.find((person) => person.name.toLowerCase() == newperson.name)) {
+      resp.status(404).send({ error: "person already exists" });
+    } else {
+      persons = persons.concat(newperson);
+      resp.sendStatus(204).end();
+      console.log(JSON.stringify(newperson));
+    }
+  }
+});
+
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
