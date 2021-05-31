db231ff821f223e11ab30698deaa1c7adb1e7bfd
eafds <eafds@gmail.com>
2021-06-01 02:31:06 +0530

    relative api

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const baseurl = "http://localhost:3001/api/persons";
+const baseurl = "/api/persons";
+
 
 const getPersons = () => {
   const request = axios.get(baseurl);
