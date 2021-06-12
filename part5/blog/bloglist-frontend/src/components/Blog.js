88008b9368d769b7d434f2f2364725f102fe17a6
phaggy <rynmalik@gmail.com>
2021-06-12 12:51:38 +0530

    5.6,5.7* Blog list frontend, step7

--- part5/blog/bloglist-frontend/src/components/Blog.js
+++ part5/blog/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Togglable from "./toggelable";
 const Blog = ({ blog }) => (
-  <div>
-    <p>
+  <div className="blog">
+    <div className="compact-blog">
       {blog.title} {blog.author}
-    </p>
+    </div>
+    <Togglable buttonLabel="view">
+      <div>
+        <div>{blog.url}</div>
+        <div>
+          Likes: {blog.likes} <button>Like</button>
+        </div>
+        <div>{blog.user ? blog.user.name : null}</div>
+      </div>
+    </Togglable>
   </div>
 );
 
